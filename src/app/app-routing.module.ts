c6223fac2e9fdd0799779247ae6f2fca72da873b
ramiromendozag <103544156+ramiromendozag@users.noreply.github.com>
2023-03-28 23:28:13 -0400

    actualizar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'website', loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule) }];
+const routes: Routes = [{ 
+  path: 'website', loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule), 
+  { path: '', loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule) },
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
